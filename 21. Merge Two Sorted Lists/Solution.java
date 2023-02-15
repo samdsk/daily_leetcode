731a949ba77c12a17e2667bc1b1b5804dab4a459
Sam. K <sam.ds@live.com>
2023-02-15 10:29:52 +0100

    recursive version

--- 21. Merge Two Sorted Lists/Solution.java
+++ 21. Merge Two Sorted Lists/Solution.java
@@ -11,44 +11,19 @@
 
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head = new ListNode();
+        if(list1 == null || list2 == null)
+            return list1 == null ? list2 : list1;
 
-        if(list1 == null && list2 == null) return null;
+        ListNode list;
 
-        ListNode output = head;
-
-        while(list1 != null && list2 != null){
-            int temp;
-            if(list1.val<=list2.val){
-                temp = list1.val;
-                
-                list1 = list1.next;
-                
-            }else{
-                temp = list2.val;
-                list2 = list2.next;
-                
-            }
-            System.out.println(temp);
-            head.val = temp;
-            head.next = (list2 == null && list1 == null) ? null : new ListNode();
-            head = head.next;
+        if(list1.val < list2.val){            
+            list = new ListNode(list1.val);
+            list.next = mergeTwoLists(list1.next, list2);
+        }else{
+            list = new ListNode(list2.val);
+            list.next = mergeTwoLists(list1, list2.next);
         }
 
-        while(list1!= null){
-            head.val = list1.val;
-            head.next = (list1.next == null) ? null : new ListNode();
-            head = head.next;
-            list1 = list1.next;
-        }
-
-        while(list2 != null){
-            head.val = list2.val;            
-            head.next = (list2.next == null) ? null : new ListNode();
-            head = head.next;
-            list2 = list2.next;
-        }
-
-        return output;
+        return list;
     }
 }

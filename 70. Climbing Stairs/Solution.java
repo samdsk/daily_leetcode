0a9cf6b6b9fcd4b5ed9491bae464da7a4e0c7a24
Sam. K <sam.ds@live.com>
2023-03-05 17:38:11 +0100

    70. Climbing Stairs

---
+++ 70. Climbing Stairs/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=1) return 1;
+
+        int prev1 = 1;
+        int prev2 = 2;
+        int cur = 0;
+        for (int i = 3; i <= n; i++) {
+            cur = prev1+prev2;
+            prev1 = prev2;
+            prev2 = cur;
+        }
+
+        return cur;
+    }
+}

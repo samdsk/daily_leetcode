3f03ed57b934fcf2cf8a5bf40533227c34ecbaf3
Sam. K <sam.ds@live.com>
2023-03-14 09:10:34 +0100

    129. Sum Root to Leaf Numbers - first version (slow)

---
+++ 129. Sum Root to Leaf Numbers/Solution.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    private int sum;
+
+    public int sumNumbers(TreeNode root) {
+        if(root == null) return 0;
+        this.sum = 0;
+
+        helper(new StringBuilder(), root);
+
+        return sum;
+    }
+
+    private void helper(StringBuilder number, TreeNode root){
+        if(root == null) {
+            return;
+        }        
+        StringBuilder temp = new StringBuilder(number);
+        temp.append(root.val);
+
+        if(root.left == null && root.right == null){
+            sum += Integer.parseInt(temp.toString());
+            return;
+        }
+
+        helper(temp, root.left);
+        helper(temp, root.right);
+
+
+        return;
+
+    }
+}

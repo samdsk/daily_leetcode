e3a84ea73a6b32ef7e8fffe92181b88bbd516036
Sam. K <sam.ds@live.com>
2023-03-20 10:56:25 +0100

    605. Can Place Flowers

--- 605. Can Place Flowers/Solution.java
+++ 605. Can Place Flowers/Solution.java
@@ -1,5 +1,24 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int max = 0;
         
+        for(int i=0;i<flowerbed.length; i++){
+
+            if(flowerbed[i]==0){
+                boolean leftOk = i == 0 || flowerbed[i-1] == 0;
+                boolean rightOk = i == flowerbed.length-1 || flowerbed[i+1] == 0;
+
+                if(leftOk && rightOk){
+                    flowerbed[i] = 1;
+                    max++;
+                }
+            }
+        }
+
+        return max>=n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().canPlaceFlowers(new int[]{1,0,0,0,1}, 1));
     }
 }

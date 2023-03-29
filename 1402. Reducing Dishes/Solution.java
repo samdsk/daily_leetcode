a2b4439b6ae5b0a58f439ace55923fb6ab380cd9
Sam. K <sam.ds@live.com>
2023-03-29 09:10:56 +0200

    1402. Reducing Dishes : fastest

--- 1402. Reducing Dishes/Solution.java
+++ 1402. Reducing Dishes/Solution.java
@@ -1,35 +1,35 @@
 import java.util.Arrays;
 
 class Solution {
-    public int maxSatisfaction(int[] satisfaction) {
-        Arrays.sort(satisfaction);
-        int n = satisfaction.length;
-        int[] prev = new int[n + 2];
-        Arrays.fill(prev,0);
-        for (int i = n-1; i>=0; i--) {
-
-            int[] dp = new int[n+2];
-
-            for (int time = 1; time < n; time++) {
-                dp[time] = Math.max(satisfaction[i]*time + prev[time+1],prev[time]);
-            }
-
-            prev = dp;
-        }
-
-        return prev[1];
-    }
     // public int maxSatisfaction(int[] satisfaction) {
     //     Arrays.sort(satisfaction);
     //     int n = satisfaction.length;
-
-    //     int sum = 0,cur = 0,ans = 0;
-
+    //     int[] prev = new int[n + 2];
+    //     Arrays.fill(prev,0);
     //     for (int i = n-1; i>=0; i--) {
-    //         cur += sum + satisfaction[i];
-    //         sum += satisfaction[i];
-    //         ans = Math.max(ans,cur);
+
+    //         int[] dp = new int[n+2];
+
+    //         for (int time = 1; time < n; time++) {
+    //             dp[time] = Math.max(satisfaction[i]*time + prev[time+1],prev[time]);
+    //         }
+
+    //         prev = dp;
     //     }
-    //     return ans;
+
+    //     return prev[1];
     // }
+    public int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
+        int n = satisfaction.length;
+
+        int sum = 0,cur = 0,ans = 0;
+
+        for (int i = n-1; i>=0; i--) {
+            cur += sum + satisfaction[i];
+            sum += satisfaction[i];
+            ans = Math.max(ans,cur);
+        }
+        return ans;
+    }
 }

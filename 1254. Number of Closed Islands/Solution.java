d321c8a3f86606c8195130c83d96c26e8fa16b40
Sam. K <sam.ds@live.com>
2023-04-06 08:30:09 +0200

    1254. Number of Closed Islands : dfs optimized

--- 1254. Number of Closed Islands/Solution.java
+++ 1254. Number of Closed Islands/Solution.java
@@ -5,16 +5,51 @@ class Solution {
     public int closedIsland(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][] visit = new boolean[m][n];
-        int count = 0;
+
+        // BFS
+        // boolean[][] visit = new boolean[m][n];
+        // int count = 0;
+
+        // for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if(grid[i][j] == 0 && !visit[i][j] && bfs(i,j,m,n,grid,visit))
+        //             count++;
+        //     }
+        // }
+
+        // DFS with marking directly on grid the visited cell with "-1"
+        for (int i = 0; i < m; i++) {
+            dfs(grid,i,0,m,n);
+            dfs(grid,i,n-1,m,n);
+        }
+        for (int i = 0; i < n; i++) {
+            dfs(grid,0,i,m,n);
+            dfs(grid,m-1,i,m,n);
+        }
+
+        int islands = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if(grid[i][j] == 0 && !visit[i][j] && bfs(i,j,m,n,grid,visit))
-                    count++;
+                if(grid[i][j] == 0){
+                    islands++;
+                    dfs(grid,i,j,m,n);
+                }
             }
         }
-        return count;
+
+        return islands;
+
+    }
+    private void dfs(int[][] grid, int r, int c,int m,int n) {
+        if(r<0 || r>=m || c<0 || c>=n || grid[r][c] != 0) return;
+
+        grid[r][c] = -1;
+
+        dfs(grid,r+1,c,m,n);
+        dfs(grid,r,c+1,m,n);
+        dfs(grid,r-1,c,m,n);
+        dfs(grid,r,c-1,m,n);
     }
     private boolean bfs(int x,int y,int m,int n,int[][] grid,boolean[][] visit){
         Queue<int[]> q = new LinkedList<>();

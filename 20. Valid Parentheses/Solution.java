3a1068e528950aeb433d8430607a9809b7a51e30
Sam. K <sam.ds@live.com>
2023-04-10 10:00:52 +0200

    20. Valid Parentheses

---
+++ 20. Valid Parentheses/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                stack.push(')');
+            }else if(c == '['){
+                stack.push(']');
+            }else if(c == '{'){
+                stack.push('}');
+            }
+
+            if(stack.isEmpty() || stack.pop() != c)
+                return false;
+
+
+        }
+
+        return stack.isEmpty();
+    }
+}

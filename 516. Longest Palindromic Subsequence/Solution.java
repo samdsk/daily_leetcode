1fcd25d320559f53069e4d328dc320f5e203a32a
Sam. K <sam.ds@live.com>
2023-04-14 09:23:57 +0200

    516. Longest Palindromic Subsequence : dp

--- 516. Longest Palindromic Subsequence/Solution.java
+++ 516. Longest Palindromic Subsequence/Solution.java
@@ -1,10 +1,40 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
+        char[] arr = s.toCharArray();
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
+        for (int i = n-1; i>=0; i--) {
+            for (int j = i+1; j<n; j++) {
+                if(arr[i] == arr[j])
+                    dp[i][j] = 2 + dp[i+1][j-1];
+                else dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[0][n-1];
+
         // int n = s.length();
         // int[] dp = new int[n];
         // int[] dpPrev = new int[n];
 
-        return find(0, s.length()-1, s);
+        // for (int i = n-1; i>=0; --i) {
+        //     dp[i] = 1;
+        //     for (int j = i+1; j<n; ++j) {
+        //         if(s.charAt(i) == s.charAt(j)){
+        //             dp[j] = dpPrev[j-1]+2;
+        //         }else{
+        //             dp[j] = Math.max(dpPrev[j], dp[j-1]);
+        //         }
+        //     }
+        //     dpPrev = dp.clone();
+        // }
+
+        // return dp[n-1];
     }
 
     private int find(int i, int j, String s){

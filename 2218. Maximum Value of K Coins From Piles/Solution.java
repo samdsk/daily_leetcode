59a11d5b76090b78f43f5bb8d07a8193a8b9e365
Sam. K <sam.ds@live.com>
2023-04-15 12:45:51 +0200

    fix

--- 2218. Maximum Value of K Coins From Piles/Solution.java
+++ 2218. Maximum Value of K Coins From Piles/Solution.java
@@ -6,8 +6,8 @@ class Solution {
         int[][] dp = new int [n+1][k+1];
 
         for (int i = 1; i<=n; i++) {
-            for (int coins = 0; coins < k; coins++) {
-                int sum  = 0;
+            for (int coins = 0; coins <= k; coins++) {
+                int sum = 0;
                 for (int currentCoins = 0; currentCoins<= Math.min((int) piles.get(i-1).size(),coins); currentCoins++) {
                     if(currentCoins>0){
                         sum += piles.get(i-1).get(currentCoins - 1);

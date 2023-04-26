0755e8e24c68ab1ec1ccb8ccdfa2a2599b98e799
Sam. K <sam.ds@live.com>
2023-04-26 07:57:47 +0200

    258. Add Digits : java + py

---
+++ 258. Add Digits/Solution.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int addDigits(int num) {
+        if(num == 0) return 0;
+        return 1 + ((num-1) % 9);
+    }
+}
---
+++ 258. Add Digits/Solution.py
@@ -0,0 +1,4 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0 : return 0
+        return 1 + ((num-1) % 9)

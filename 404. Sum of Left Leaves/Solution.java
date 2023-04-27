3175e74139239f3dbd765e5c3c168534df1a7237
Sam. K <sam.ds@live.com>
2023-04-27 17:14:11 +0200

    404. Sum of Left Leaves

---
+++ 404. Sum of Left Leaves/Solution.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    private int sum = 0;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        dfs(root);
+        return sum;
+    }
+
+    private void dfs(TreeNode root){
+        if(root == null) return;
+        if(root.left != null && root.left.left == null && root.left.right == null) sum += root.left.val;
+        dfs(root.left);
+        dfs(root.right);
+    }
+}
+
+
+// Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

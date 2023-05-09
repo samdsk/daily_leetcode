e3a4c94ef2745da634e6ce6128d8fb91e9e836ee
Sam. K <sam.ds@live.com>
2023-05-09 08:34:55 +0200

    2634. Filter Elements from Array

---
+++ 2634. Filter Elements from Array/Solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    ans = []
+    for(i = 0;i<arr.length;i++){
+        if(fn(arr[i],i)) ans.push(arr[i])
+    }
+
+    return ans
+};

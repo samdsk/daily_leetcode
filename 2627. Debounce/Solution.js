11e55e92766f5c4c19fa36dafef925029ec86d94
Sam. K <sam.ds@live.com>
2023-05-19 13:53:10 +0200

    2627. Debounce

---
+++ 2627. Debounce/Solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let interval
+    return function(...args) {
+        const lastCall = Date.now()
+        clearInterval(interval)
+
+        interval = setInterval(() => {
+            if(Date.now() - lastCall >= t){
+                fn(...args)
+                clearInterval(interval)
+            }
+        }, 1);
+
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */

f9f04c41ae568fb0947bcd593a30265f268c7196
sam-linux <sam.ds@live.com>
2023-05-21 09:16:15 +0200

    2628. JSON Deep Equal

--- 2628. JSON Deep Equal/Solution.js
+++ 2628. JSON Deep Equal/Solution.js
@@ -4,21 +4,26 @@
  * @return {boolean}
  */
 var areDeeplyEqual = function(o1, o2) {
-    if(o1 === o2) return true
-    if(o1.length != o2.length) return false
+    if(o1 === o2) return true;
+    if(o1 === null || o2 === null) return false
+    if(String(o1) !== String(o2)) return false
+
+    if(typeof o1 !== 'object') return o1 === o2
 
     if(Array.isArray(o1)){
-        for(let i = 0;i<o1.length;i++){
-            if(o1[i] !== o2[i]) return false
+        if(o1.length !== o2.length) return false
+
+        for(let i = 0;i<o1.length; i++) {
+            if(!areDeeplyEqual(o1[i],o2[i])) return false
         }
+
+        return true
     }
 
+    if(Object.keys(o1).length !== Object.keys(o2).length) return false
 
-    for(key of Object.keys(o1)){
-        if(o2[key] === undefined) return false
-        if(o1[key] !== o2[key]) return false
-        if(o1[key] === 'object' && !areDeeplyEqual(o1[key],o2[key])) return false
-
+    for(const key in o1){
+        if(!areDeeplyEqual(o1[key],o2[key])) return false
     }
 
     return true

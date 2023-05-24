0fe50f5ca650c04cc3b12703a37835f4abebdd65
Sam. K <sam.ds@live.com>
2023-05-24 07:01:40 +0200

    2700. Differences Between Two Objects

---
+++ 2700. Differences Between Two Objects/Solution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {object} obj1
+ * @param {object} obj2
+ * @return {object}
+ */
+function objDiff(obj1, obj2) {
+    if(obj1 === obj2) return {}
+    if(obj1 === null || obj2 === null) return [obj1,obj2]
+
+    if(typeof obj1 !== 'object' || typeof obj2 !== 'object') return [obj1,obj2]
+    if(Array.isArray(obj1) !== Array.isArray(obj2)) return [obj1,obj2]
+
+    const ans = {}
+
+    for(const key in obj1){
+        if(key in obj2){
+            const diff = objDiff(obj1[key],obj2[key])
+            if(Object.keys(diff).length > 0){
+                ans[key] = diff
+            }
+        }
+    }
+
+    return ans
+};

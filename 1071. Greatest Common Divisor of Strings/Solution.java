43874dca23d9e27ffd9183aecd68a5e170009562
Sam. K <sam.ds@live.com>
2023-06-04 09:17:25 +0200

    1071. Greatest Common Divisor of Strings

---
+++ 1071. Greatest Common Divisor of Strings/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)) return "";
+
+        int remain = gcd(str1.length(),str2.length());
+
+        return str1.substring(0, remain);
+    }
+
+    private int gcd(int a, int b) {
+        if(b == 0) return a;
+        return gcd(b, a%b);
+    }
+
+
+}

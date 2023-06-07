caaf9b26f36ad492f0e7160a327b97a8694db21e
Sam. K <sam.ds@live.com>
2023-06-07 07:52:55 +0200

    1318. Minimum Flips to Make a OR b Equal to c

---
+++ 1318. Minimum Flips to Make a OR b Equal to c/Solution.java
@@ -0,0 +1,27 @@
+// class Solution {
+//     public int minFlips(int a, int b, int c) {
+//         int ans = 0;
+
+//         while(a != 0 | b != 0 | c != 0){
+//             if((c&1) == 1){
+//                 if((a&1)==0 && (b&1)==0){
+//                     ans++;
+//                 }
+//             }else{
+//                 ans += (a&1) + (b&1);
+//             }
+
+//             a >>= 1;
+//             b >>= 1;
+//             c >>= 1;
+//         }
+
+//         return ans;
+//     }
+// }
+
+class Solution {
+    public int minFlips(int a, int b, int c) {
+        return Integer.bitCount((a | b) ^ c) + Integer.bitCount(a & b & ((a | b) ^ c));
+    }
+}

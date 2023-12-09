b63b142bb03d545b699ee78a37861cfd73dee90b
Sam. K. <sam.ds@live.com>
2023-12-09 08:49:16 +0100

    94. Binary Tree Inorder Traversal

--- 94. Binary Tree Inorder Traversal/Solution.java
+++ 94. Binary Tree Inorder Traversal/Solution.java
@@ -15,17 +15,18 @@
  */
 class Solution {
     private List<Integer> ans = new ArrayList<>();
-      public List<Integer> inorderTraversal(TreeNode root) {
-          dfs(root);
-          return ans;
-      }
-  
-      private void dfs(TreeNode root){
-        if(root == null) return;
-  
-        dfs(root.left);
-        ans.add(root.val);
-        dfs(root.right);
-        
-      } 
-  }
+    
+    public List<Integer> inorderTraversal(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    private void dfs(TreeNode root){
+    if(root == null) return;
+
+    dfs(root.left);
+    ans.add(root.val);
+    dfs(root.right);
+    
+    } 
+}

893874eca2e96bfbe70da261389e1a8da6766526
Sam. K. <sam.ds@live.com>
2024-06-27 17:14:29 +0200

    1251. Average Selling Price

---
+++ 1251. Average Selling Price/Solution.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+select p.product_id, COALESCE(round(sum(p.price * u.units)::numeric / sum(u.units),2),0) as average_price
+from Prices p 
+left join UnitsSold u 
+on p.product_id = u.product_id and u.purchase_date between p.start_date and p.end_date
+group by p.product_id
